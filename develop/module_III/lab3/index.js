743391b1c65205be916c4daba796824ef55a77a3
hectoregar <hectoregar@gmail.com>
2024-07-03 14:07:14 -0500

    Se incluye el Lab3 Factory
    Se incluye en los Ejércicios Prácticos el Lab3 ejercicio 4 de Factory

---
+++ develop/module_III/lab3/index.js
@@ -0,0 +1,15 @@
+const Member = require("./Member");
+const User = require("./User");
+const Factory =require("./Factory");
+
+let user =new User('hector123@gmail.com', 'Hector', 'Garcia', 'CC', '123456', '60145678', 'calle feliz', '123*..' );
+user.displayInfo();
+user.updatetypeDocument("Registro_Internacional");
+user.displayInfo();
+
+// Crear instancia de Roll de usuario
+
+let admin = new Member('hector123@gmail.com', 'Hector', 'Garcia', 'CC', '123456', '60145678', 'calle feliz', '123*..', '01', 'invitado' )
+admin.displayInfo();
+admin.updatetypeDocument("Registro internacional de identificación");
+admin.displayInfo();
